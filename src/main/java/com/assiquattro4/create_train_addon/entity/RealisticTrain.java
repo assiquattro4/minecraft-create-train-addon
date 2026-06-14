8e92e530677407cb19a70e225624bc8643e7a329
assiquattro4 <lorenzo.montagna.2009.01@gmail.com>
2026-06-14 23:30:15 +0200

    Update RealisticTrain.java

--- src/main/java/com/assiquattro4/create_train_addon/entity/RealisticTrain.java
+++ src/main/java/com/assiquattro4/create_train_addon/entity/RealisticTrain.java
@@ -1,49 +1,34 @@
 package com.assiquattro4.create_train_addon.entity;
 
-import com.assiquattro4.create_train_addon.physics.PhysicsCalculator;
-import com.assiquattro4.create_train_addon.physics.PhysicsStateMachine;
+// 1. Importa il profilo
+import com.assiquattro4.create_train_addon.physics.TrainPhysicsProfile;
 import com.simibubi.create.content.trains.entity.Train;
-import net.minecraft.world.phys.Vec3;
+import net.minecraft.nbt.CompoundTag;
 
 public class RealisticTrain extends Train {
-    
-    // Il profilo fisico (il "DNA" del treno)
+
+    // 2. Dichiarazione dell'istanza
     public final TrainPhysicsProfile physicsProfile;
 
     public RealisticTrain(UUID id, UUID owner) {
         super(id, owner);
-        // Inizializza con valori standard
+        // 3. Inizializzazione
         this.physicsProfile = new TrainPhysicsProfile();
-        System.out.println("Treno Fisico istanziato con successo: " + id);
     }
-    
-    // Ricorda di aggiungere i metodi read/write per salvare physicsProfile
-}
 
-@Override
-public void tick() {
-    super.tick();
-
-    // Se siamo sul server, calcoliamo la fisica
-    if (!this.level.isClientSide) {
-        // Passiamo 'this' per avere accesso a tutti i dati del treno
-        var forces = PhysicsCalculator.calculateForces(this);
-        
-        // Applichiamo la fisica
-        Vec3 pos = this.carriages.get(0).getAnchorPosition();
-        PhysicsStateMachine.applyPhysicsState(forces.netThrust(), this.getTotalMass(), pos, this.level);
-        
-        // Modifica la velocità
-        this.speed += (forces.netThrust() / this.getTotalMass());
+    @Override
+    public void write(CompoundTag tag) {
+        super.write(tag);
+        // Salva usando l'istanza
+        tag.put("PhysicsProfile", this.physicsProfile.save());
     }
-}
 
-// Helper per calcolare la massa totale (Create non lo fa nativamente per tutte le carrozze)
-public double getTotalMass() {
-    double totalMass = 0;
-    for (var carriage : this.carriages) {
-        // Assumendo che ogni carrozza abbia un peso base + peso contenuto
-        totalMass += 1000.0; // Esempio: 1000 unità di massa per carrozza
+    @Override
+    public void read(CompoundTag tag) {
+        super.read(tag);
+        // Carica usando l'istanza
+        if (tag.contains("PhysicsProfile")) {
+            this.physicsProfile.load(tag.getCompound("PhysicsProfile"));
+        }
     }
-    return totalMass;
 }
